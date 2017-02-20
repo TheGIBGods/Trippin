a38b169a5aaeeac1791ba275103fdf966f919df7
BrageD <brage.dimm@hotmail.com>
2017-02-20 16:09:55 +0100

    Fix't tilgang til node_modules

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')));
 app.use('/', index);
 app.use('/users', users);
 
