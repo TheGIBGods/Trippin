21f79575c9d91f71a8a8c400df1c2c755e4c3a98
Guro Brøto <guro.g.broto@gmail.com>
2017-02-23 15:17:31 +0100

    Added module for trip

---
+++ models/trip.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Guro on 23.02.2017.
+ */
+//Require Mongoose
+var mongoose = require('mongoose');
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var TripSchema = new Schema({
+    name: {type: String, required: true},
+    date: Date,
+    comment: String,
+    users: [{type: Schema.Types.ObjectId, ref: 'User'}]
+});
+
+
+module.exports = mongoose.model('Trip', TripSchema);
