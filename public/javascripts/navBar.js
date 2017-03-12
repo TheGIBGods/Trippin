cab8f3b83cae9331be402d71bcce6221f7825c04
Synne Fog <synne.fog@teknologiporten.no>
2017-03-12 13:01:03 +0100

    navigate in navBar

--- public/javascripts/navBar.js
+++ public/javascripts/navBar.js
@@ -4,3 +4,15 @@
 $(document).ready(function userNameNav() {
     document.getElementById("#userName").innerHTML = getUser();
 });
+
+function logOut(){
+    console.log("in logout");
+    setUser(null);
+    console.log(getUser());
+}
+
+function changeTrip() {
+    console.log("in changeTrip");
+    setTrip(null);
+    console.log(getTrip());
+}
