6e8b98c2f2845132bbe11519d9cebfd5b42c55b2
Guro Brøto <guro.g.broto@gmail.com>
2017-04-24 16:42:56 +0200

    sidebarfix

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -8,44 +8,167 @@ function filterMarkers(){
     firstIteration = true;
 
     if(document.getElementById("activityCB").checked){
-        console.log( "IN ACTIVITY");
-        for (i = 0; categories[0].length  > i; i++){
-            checkedMarkers.push(categories[0][i]);
+        setPointsOnMap(categories[0]);
+        document.getElementById("activity").className = "categories canBeClicked showList";
+        document.getElementById("activity_list").className = "hideThis";
+        console.log("Adding eventlistener");
+        if(!document.getElementById("activity").getAttribute("class").includes("showList")){
+            document.getElementById("activity").addEventListener("click", function(){
+                console.log("add");
+                enableToggle(event.target)
+            });
         }
+
+
+    }else{
+        document.getElementById("activity").className = "categories  makeShowLess";
+        document.getElementById("activity_list").className = "hideThis";
+        console.log("Removing event listener");
+        document.getElementById("activity").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
     }
     if(document.getElementById("attractionCB").checked){
-        for (i = 0; categories[1].length  > i; i++){
-            checkedMarkers.push(categories[1][i]);
-        }    }
+        setPointsOnMap(categories[1]);
+        document.getElementById("attractions").className = "categories canBeClicked showList";
+        document.getElementById("attractions_list").className = "";
+        if(!document.getElementById("attractions").getAttribute("class").includes("showList")){
+            document.getElementById("attractions").addEventListener("click", function(){
+                enableToggle(event.target)
+                console.log("add");
+            });
+        }
+
+    }else{
+        document.getElementById("attractions").className = "categories  makeShowLess";
+        document.getElementById("attractions_list").className = "hideThis";
+        document.getElementById("attractions").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
+    }
     if(document.getElementById("hotelCB").checked){
-        for (i = 0; categories[2].length  > i; i++){
-            checkedMarkers.push(categories[2][i]);
-        }    }
+        setPointsOnMap(categories[2]);
+        document.getElementById("hotels").className = "categories canBeClicked showList";
+        document.getElementById("hotels_list").className = "";
+        if(!document.getElementById("hotels").getAttribute("class").includes("showList")){
+            document.getElementById("hotels").addEventListener("click", function(){
+                enableToggle(event.target)
+                console.log("add");
+            });
+        }
+
+    }else{
+        document.getElementById("hotels").className = "categories  makeShowLess";
+        document.getElementById("hotels_list").className = "hideThis";
+        document.getElementById("hotels").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
+    }
     if(document.getElementById("restaurantCB").checked){
-        for (i = 0; categories[4].length > i; i++){
-            checkedMarkers.push(categories[4][i]);
-        }    }
+        setPointsOnMap(categories[4]);
+        document.getElementById("restaurants").className = "categories canBeClicked showList";
+        document.getElementById("restaurants_list").className = " ";
+        if(!document.getElementById("restaurants").getAttribute("class").includes("showList")){
+            document.getElementById("restaurants").addEventListener("click", function(){
+                enableToggle(event.target)
+                console.log("add");
+            });
+        }
+
+    }else{
+        document.getElementById("restaurants").className = "categories  makeShowLess";
+        document.getElementById("restaurants_list").className = "hideThis";
+        document.getElementById("restaurants").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
+    }
     if(document.getElementById("shoppingCB").checked){
-        for (i = 0; categories[3].length > i; i++){
-            checkedMarkers.push(categories[3][i]);
-        }    }
+        setPointsOnMap(categories[3]);
+        document.getElementById("shopping").className = "categories canBeClicked showList";
+        document.getElementById("shopping_list").className = "";
+        if(!document.getElementById("shopping").getAttribute("class").includes("showList")){
+            document.getElementById("shopping").addEventListener("click", function(){
+                enableToggle(event.target)
+                console.log("add");
+            });
+        }
+
+    }else{
+        document.getElementById("shopping").className = "categories  makeShowLess";
+        document.getElementById("shopping_list").className = "hideThis";
+        document.getElementById("shopping").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
+    }
     if(document.getElementById("transportationCB").checked){
-        for (i = 0; categories[5].length > i; i++){
-            checkedMarkers.push(categories[5][i]);
-        }    }
+        setPointsOnMap(categories[5]);
+        document.getElementById("transportation").className = "categories canBeClicked showList";
+        document.getElementById("transportation_list").className = "";
+        if(!document.getElementById("transportation").getAttribute("class").includes("showList")){
+            document.getElementById("transportation").addEventListener("click", function(){
+                enableToggle(event.target)
+                console.log("add");
+            });
+        }
+
+    }else{
+        document.getElementById("transportation").className = "categories  makeShowLess";
+        document.getElementById("transportation_list").className = "hideThis";
+        document.getElementById("transportation").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
+    }
     if(document.getElementById("otherCB").checked){
-        for (i = 0; categories[6].length > i; i++){
-            checkedMarkers.push(categories[6][i]);
-        }    }
+        setPointsOnMap(categories[6]);
+        document.getElementById("other").className = "categories canBeClicked showList";
+        document.getElementById("other_list").className = "";
+        if(!document.getElementById("other").getAttribute("class").includes("showList")){
+            document.getElementById("other").addEventListener("click", function(){
+                enableToggle(event.target)
+                console.log("add");
+            });
+        }
+
+    }else{
+        document.getElementById("other").className = "categories  makeShowLess";
+        document.getElementById("other_list").className = "hideThis";
+        document.getElementById("other").removeEventListener("click", function(){
+            enableToggle(event.target)
+            console.log("remove");
+        });
+    }
     if(document.getElementById("allCB").checked){
         for (var i = 0; i < categories.length; i++) {
-            setPointsOnMap(categories[i], false);
+            setPointsOnMap(categories[i]);
+            document.getElementById("activity").className = "categories canBeClicked showList";
+            document.getElementById("attractions").className = "categories canBeClicked showList";
+            document.getElementById("hotels").className = "categories canBeClicked showList";
+            document.getElementById("restaurants").className = "categories canBeClicked showList";
+            document.getElementById("shopping").className = "categories canBeClicked showList";
+            document.getElementById("transportation").className = "categories canBeClicked showList";
+            document.getElementById("other").className = "categories canBeClicked showList";
+
+            document.getElementById("activity_list").className = "";
+            document.getElementById("attractions_list").className = "";
+            document.getElementById("hotels_list").className = "";
+            document.getElementById("restaurants_list").className = "";
+            document.getElementById("shopping_list").className = "";
+            document.getElementById("transportation_list").className = "";
+            document.getElementById("other_list").className = "";
+
         }
     }
     else{
         console.log(checkedMarkers);
         setPointsOnMap(checkedMarkers, false);
     }
+
 
 
     $('#modalFilter').modal('toggle');
--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -4,6 +4,36 @@
 
 $(document).ready(function(){
 
+    document.getElementById("activity").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+
+    document.getElementById("attractions").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+
+    document.getElementById("hotels").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+
+    document.getElementById("restaurants").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+
+    document.getElementById("shopping").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+
+    document.getElementById("transportation").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+
+    document.getElementById("other").addEventListener("click", function(){
+        enableToggle(event.target)
+    });
+    //startToggling();
+
+    /*
     //Category navigation in sidabar
     $("#activity").click(function(){
         $("#activity_list").slideToggle(200);
@@ -34,5 +64,30 @@ $(document).ready(function(){
     });
     //end category navigation
 
+    */
 
 });
+
+function startToggling() {
+    //document.getElementById("activity").addEventListener("click", enableToggle(event.target.id));
+
+    var lists = $(".showList");
+
+    for(i = 0; i< lists.length; i++){
+        lists[i].addEventListener("click", function () {
+            console.log("event.target:")
+            console.log(event.target);
+            enableToggle(event.target.id);
+        })
+    }
+}
+
+function enableToggle(el) {
+    if(el.getAttribute("class").includes("showList")){
+        var id = el.id;
+        var s = "" + id +"_list";
+        console.log("id to be toggled: " + s);
+        $("#" + s).slideToggle(200);
+    }
+
+}
