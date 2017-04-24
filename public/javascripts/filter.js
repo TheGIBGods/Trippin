5fc1f035fc78c810ac231b3a54b6b4b2c6c47adb
BrageD <brage.dimm@hotmail.com>
2017-04-24 13:13:48 +0200

    zum whenn changing category

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -3,41 +3,55 @@
  */
 
 function filterMarkers(){
+    checkedMarkers = new Array();
     clearMap();
     firstIteration = true;
 
     if(document.getElementById("activityCB").checked){
-        setPointsOnMap(categories[0]);
-    }
-    if(document.getElementById("attractionCB").checked){
-        setPointsOnMap(categories[1]);
-    }
-    if(document.getElementById("hotelCB").checked){
-        setPointsOnMap(categories[2]);
-    }
-    if(document.getElementById("restaurantCB").checked){
-        setPointsOnMap(categories[4]);
-    }
-    if(document.getElementById("shoppingCB").checked){
-        setPointsOnMap(categories[3]);
-    }
-    if(document.getElementById("transportationCB").checked){
-        setPointsOnMap(categories[5]);
-    }
-    if(document.getElementById("otherCB").checked){
-        setPointsOnMap(categories[6]);
-    }
-    if(document.getElementById("allCB").checked){
-        for (var i = 0; i < categories.length; i++) {
-            setPointsOnMap(categories[i]);
+        console.log( "IN ACTIVITY");
+        for (i = 0; categories[0].length  > i; i++){
+            checkedMarkers.push(categories[0][i]);
         }
     }
+    if(document.getElementById("attractionCB").checked){
+        for (i = 0; categories[1].length  > i; i++){
+            checkedMarkers.push(categories[1][i]);
+        }    }
+    if(document.getElementById("hotelCB").checked){
+        for (i = 0; categories[2].length  > i; i++){
+            checkedMarkers.push(categories[2][i]);
+        }    }
+    if(document.getElementById("restaurantCB").checked){
+        for (i = 0; categories[3].length > i; i++){
+            checkedMarkers.push(categories[3][i]);
+        }    }
+    if(document.getElementById("shoppingCB").checked){
+        for (i = 0; categories[4].length > i; i++){
+            checkedMarkers.push(categories[4][i]);
+        }    }
+    if(document.getElementById("transportationCB").checked){
+        for (i = 0; categories[5].length > i; i++){
+            checkedMarkers.push(categories[5][i]);
+        }    }
+    if(document.getElementById("otherCB").checked){
+        for (i = 0; categories[6].length > i; i++){
+            checkedMarkers.push(categories[6][i]);
+        }    }
+    if(document.getElementById("allCB").checked){
+        for (var i = 0; i < categories.length; i++) {
+            setPointsOnMap(categories[i], false);
+        }
+    }
+    else{
+        console.log(checkedMarkers);
+        setPointsOnMap(checkedMarkers, false);
+    }
+
 
     $('#modalFilter').modal('toggle');
 }
 
-function clearMap(){
-    console.log("trying to clear")
+function clearMap(){ 
     //mymap.removeLayer(markersOnMap);
     for(var i = 0; i < markersOnMap.length; i++) {
         mymap.removeLayer(markersOnMap[i]);
